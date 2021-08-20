c2e616ebd9c866da40850abcfb5085bd8981b3b7
luis botelho <luisfellypebotelho@gmail.com>
2021-08-20 15:55:29 -0300

    Finalizadno projeto

--- treino01/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-
-const modeloCarro = ['onix', 'uno', 'toro']
-const tipoCarro = ['SUV', 'Sedan', 'Hatch']
-const conbustivel = ['alcool', 'gasolina','flex','diesel']
-const marca = ['chevrolet','fiat','citroen']
-const ano = [1970, 2005, 2015]
