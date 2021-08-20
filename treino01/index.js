2ea7f9483aebae89dd324b15803c85adb78723d0
luis botelho <luisfellypebotelho@gmail.com>
2021-08-19 23:06:17 -0300

    Criadnod o CRUD

---
+++ treino01/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const modeloCarro = ['onix', 'uno', 'toro']
+const tipoCarro = ['SUV', 'Sedan', 'Hatch']
+const conbustivel = ['']
